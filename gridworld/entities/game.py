cad7459c4a6bca362f9019a308f4baceea5a433e
Gouher Danish <gouherdanishiitkgp@gmail.com>
2024-09-13 19:12:27 +0530

    changes

--- gridworld/entities/game.py
+++ gridworld/entities/game.py
@@ -13,17 +13,13 @@ class Game:
             action = self.agent.select_action(state)
             print(state,action)
             done = False
-            i = 0
             while not done :
                 # print([(s,a,q) for (s,a), q in self.agent.estimates.items() if q.value != 0])
                 next_state, reward, done = self.env.step(state,action)
                 next_action = self.agent.select_action(next_state)
                 sarsa = (state,action.action,reward,next_state,next_action.action)
-                print(sarsa)
                 self.agent.update_estimates(*sarsa)
                 state = next_state
                 action = next_action
-                i += 1
-                if i > 100: done= True
                 print([(s,a,q) for (s,a), q in self.agent.estimates.items() if q.value != 0])
             print(f"<<<<< EPISODE {episode+1} END >>>>>")
